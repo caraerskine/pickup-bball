08322c31f42af537c9fcfe61f305a2ae76e19e8f
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-04-17 10:47:28 -0400

    created custom routes added pry gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,21 @@ Rails.application.routes.draw do
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
+
+#sessions ctrllr
+ post "/login", to: "sessions#create" 
+ delete "/logout", to: "sessions#destroy"
+
+#users ctrllr
+ post "/signup", to: "users#create" 
+ get "/me", to: "users#show"
+
+resources :games 
+resources :courts
+
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
+
+#show #me
+ #whoami shows who is logged in and who the user is aka show route not seen by user
+ #it is for react so it knows who the user is and the users show page
