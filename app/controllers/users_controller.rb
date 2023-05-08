fa34cd324f3d8eae3718743e98d0bf23d6ed1ecf
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-05-08 15:06:57 -0400

    index method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,19 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize
 
 #POST /signup
     def create
         user = User.create(user_params)
         if user.valid?
             session[:user_id] = user.id
-            render json: user
+            render json: user, status: :created
         else
-            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+            render json: { errors: user.errors.full_messages }  #, status: :unprocessable_entity
         end
+    end
+
+    def index
+        render json: User.all
     end
 
 #GET /me 
