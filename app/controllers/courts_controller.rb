d6b6eb4f55cc6044913c9b00600621830ca9c155
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-05-09 12:30:06 -0400

    added courts ctrllr

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -1,2 +1,31 @@
-class CourtsController < ApplicationController
+class CourtsController < ApplicationController        
+    before_action :authorize
+    skip_before_action :authorize, only: [:index]
+            
+        def index
+            render json: Court.all
+        end
+        
+        def create
+            court = Court.create!(bird_params)
+            render json: court, status: :created
+        end
+        
+        def show
+            court = Court.find(params[:id])
+            render json: court
+        end
+        
+        def update
+            court = Court.find(params[:id])
+            court.update!(court_params)
+            render json: court
+        end
+        
+        private
+        
+        def Court_params
+                params.permit(:park, :neighborhood, :street, :notes)
+        end        
+
 end
