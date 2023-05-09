b4e2d1c7f254957f9f3be70be8cc7860dc678207
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-05-09 10:53:02 -0400

    games ctrllr current_user

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,21 +2,20 @@ class GamesController < ApplicationController
     before_action :authorize
     before_action :authorize_user, except: [:index, :create]
    
-    #old
-    # def create
-    #     game = current_user.games.create(game_params)
-    #     if game.valid? 
-    #         render json: game
-    #     else
-    #         render json: { errors: game.errors.full_messages }, status: :unprocessable_entity
-    #     end
-    # end
-
-    #new
+    #old style
     def create
-        game = Game.create!(game_params)
+        game = current_user.games.create!(game_params)
+        render json: game, status: :created
+    end
+
+    #new thought but maybe not cuz it is going off model
+    # def create
+    #     game = Game.create!(game_params)
+    #     render json: game, status: :created
+    # end
    
-   
+
+    #old nancy style
     def index
         games = current_user.games
         render json: games
@@ -25,33 +24,41 @@ class GamesController < ApplicationController
     #work off the current_user, not the model
     #AR assoc. gives us security, it only gives "me" "mygames"
 
-   
+    #new ? not sure because this is working off the model but can i do that
+    # def index
+    #     render json: Game.all
+    # end
+
 
     def show
         game = current_user.games.find_by(id: params[:id])
-        if game 
-            render json: game 
-        else
-            render json: { error: "Not Found" }, status: :unauthorized
-        end
+        render json: game 
     end
-
-    #find by returns null if it cant find one
+      #find by returns null if it cant find one
     #if you did Game.find_by we would violate it
 
+    #new idea poss?
+    # def show
+    #     game = Game.find(params[:id])
+    #     render json: game
+    # end
+  
+
     def update
-        game = Game.find(params[:id])
+        game = current_user.find(params[:id])
         game.update!(game_params)
         render json: game
     end
     #update the user's game
+    #i am going to try current_user instead of Game 
 
     def destroy
-        game = Game.find(params[:id])
+        game = current_user.find(params[:id])
         game.destroy
         head :no_content
     end
     #delete the user's game
+    # i am going to try current_user instead of Game
 
     private
 
@@ -62,12 +69,16 @@ class GamesController < ApplicationController
 #returns current user object 
 
     def game_params
-        params.permit(:time, :bring_ball, :skill_level, :contact_info)
+        params.permit(:time, :bring_ball, :skill_level, :contact_info, :user_id)
     end
 
     def authorize
-        return render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id   
+        return render json: {error: "You are not authorized to edit this game"}, status: :unauthorized unless session.include? :user_id   
     end
+
+    #old return
+    # return render json: {error: "You are not authorized to edit this game"}, status: :unauthorized unless session.include? :user_id   
+
     #runs this method before any action happens
     #checks the condition if they are authorized, if there is an authorized user
     #if the sessions DOES not inlude user_id please do this
