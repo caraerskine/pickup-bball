74e0ac1afb4ed4f0d2fd482e3911a0b3a233bda0
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-06-08 15:49:57 -0400

    changes to notes

--- client/src/context/user.js
+++ client/src/context/user.js
@@ -286,6 +286,7 @@ export { UserContext, UserProvider };
 //any child that needs access to global state i can pass here
 //nancy video auth in rails if u need
 //parents and children
+
 //global state
 //wrap any component that needs acces to global state we wrap in consumer
 
